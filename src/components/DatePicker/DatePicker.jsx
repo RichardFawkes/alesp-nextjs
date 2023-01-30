11d70f826d851960eed3d10e45a985fd14762483
Richard <richard.@Richards-MacBook-Pro.local>
2023-01-30 15:54:36 -0300

    chore: add commit linter

---
+++ src/components/DatePicker/DatePicker.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DatePicker = () => {
+  return <div>{/* JSX code goes here */}</div>;
+};
+
+export default DatePicker;
+import React from 'react';
+
+const DatePicker = () => {
+  return (
+    <div>
+      {/* JSX code goes here */}
+    </div>
+  );
+};
+
+export default DatePicker;
---
+++ src/pages/P6/tl1.jsx
@@ -0,0 +1,12 @@
+import { BtnPrimary } from '@/components/Button/Buttons';
+
+const TL1 = () => {
+  return (
+    <div>
+      <p>TL1</p>
+      <BtnPrimary name="Botao Novo" />
+    </div>
+  );
+};
+
+export default TL1;
